b5c1562b0b3a2acd8e80b9582e3994cae625a374
id2k1149 <id2k1149@gmail.com>
2023-04-07 15:06:36 -0400

    Initial Commit

---
+++ Stopwatch_v03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stopwatch_v03
+//
+//  Created by Max Franz Immelmann on 4/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
